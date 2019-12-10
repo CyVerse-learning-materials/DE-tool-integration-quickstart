b18c9adedf183f678b631d45e8fda67f8710d5dd
upendrak <upendrakumar.devisetty@googlemail.com>
2019-12-10 13:33:37 -0700

    Update files

--- conf.py
+++ conf.py
@@ -9,11 +9,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'YOUR QUICKSTART NAME HERE as topic_quick_start'
-copyright = '2018, CyVerse'
+project = 'DE-tool-integration'
+copyright = '2019, CyVerse'
 author = 'Your Name'
-version = '1.0'
-release = '1.0'
 
 epub_title = project
 epub_author = author
